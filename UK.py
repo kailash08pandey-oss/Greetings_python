610b6c868ba0233ab9d1d8158d028051bb1d8d6b
kailash08pandey-oss <kailash08pandey@gmail.com>
2026-06-29 22:56:54 +0530

    Fix time format in UK.py to use 12-hour clock representation

--- UK.py
+++ UK.py
@@ -4,7 +4,7 @@ import pyttsx3
 import time 
 x=pyttsx3.init()
 y= time.strftime(" %D ")
-z=(time.strftime("%H""hours""%M""minutes"))
+z=(time.strftime("%I""hours""%M""minutes"))
 # checking code 
 datetim_e = "sir The date is",y,"The time is ",z
 print(y)
